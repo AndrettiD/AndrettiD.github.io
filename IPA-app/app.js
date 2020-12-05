107fa5207a1a16d9f4fd24045ec5c92dabafa8ce
Andrett Duvenhage <andrettiduv@gmail.com>
2020-12-05 14:38:09 -0800

    newAPI

--- IPA-app/app.js
+++ IPA-app/app.js
@@ -1,64 +1,16 @@
 
-    // class selector
-    document.querySelector(".newRandomBeerButton");
-    // id selector
-    document.querySelector("#randomBeer");
+$(() => {
 
-// reverance to button
-    const  newRandomBeerButton = document.querySelector("#randomBeer");
-          newRandomBeerButton.addEventListener("click", getBeer);
+      $.ajax({
+          url:"https://api.punkapi.com/v2/beers/random",
 
-          //button function
-    function getBeer(){
-     console.log("get beer button wad clicked");
-    }
+      }).then(
+         (data) =>{
+           console.log(name);
+          },
+          
+          (error)=>{
+            console.log("bad request");
+          })
 
-  const endpoint ='https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/beers/random/?key=87b934b40ebd6d2f774995a2551013d1',
-                type: "GET",
-                dataType: "json"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(() => {
-//       $.ajax({
-//           url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/beers/random/?key=87b934b40ebd6d2f774995a2551013d1',
-//           type: "GET",
-//           dataType: "json"
-//       }).then(
-//          (data) =>{
-//
-//            console.log(data);
-//           },
-//           (error)=>{
-//             console.log("bad request");
-//           })
-//
-//
-//
-//
-//     // })
-//     // })
-//
-// })
+    })
