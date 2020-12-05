7ccc9e44974d8cbd24bbbc4ade19aebc6bc2fdc3
Andrett Duvenhage <andrettiduv@gmail.com>
2020-12-04 18:06:25 -0800

    button added

--- IPA-app/app.js
+++ IPA-app/app.js
@@ -1,27 +1,64 @@
-$(() => {
-  //        $("form").on("submit",(event) => {
-  //          const userInput =
-  //
-  // $(document).ready(function() {
+
+    // class selector
+    document.querySelector(".newRandomBeerButton");
+    // id selector
+    document.querySelector("#randomBeer");
+
+// reverance to button
+    const  newRandomBeerButton = document.querySelector("#randomBeer");
+          newRandomBeerButton.addEventListener("click", getBeer);
+
+          //button function
+    function getBeer(){
+     console.log("get beer button wad clicked");
+    }
+
+  const endpoint ='https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/beers/random/?key=87b934b40ebd6d2f774995a2551013d1',
+                type: "GET",
+                dataType: "json"
 
 
-      $.ajax({
-          url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/beer/random/?key=87b934b40ebd6d2f774995a2551013d1',
-          type: "GET",
-          dataType: "json"
-      }).then(
-         (data) =>{
-
-           console.log(data);
-          },
-          (error)=>{
-            console.log("bad request");
-          })
 
 
 
 
-    // })
-    // })
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(() => {
+//       $.ajax({
+//           url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/beers/random/?key=87b934b40ebd6d2f774995a2551013d1',
+//           type: "GET",
+//           dataType: "json"
+//       }).then(
+//          (data) =>{
+//
+//            console.log(data);
+//           },
+//           (error)=>{
+//             console.log("bad request");
+//           })
+//
+//
+//
+//
+//     // })
+//     // })
+//
+// })
