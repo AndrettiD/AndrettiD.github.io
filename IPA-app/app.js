426347731b699f0113d8ce94558ffbb8dd266850
Andrett Duvenhage <andrettiduv@gmail.com>
2020-12-06 13:38:52 -0800

    html new

--- IPA-app/app.js
+++ IPA-app/app.js
@@ -1,16 +1,56 @@
 
 $(() => {
 
-      $.ajax({
-          url:"https://api.punkapi.com/v2/beers/random",
 
-      }).then(
-         (data) =>{
-           console.log(name);
-          },
-          
-          (error)=>{
-            console.log("bad request");
-          })
+        $.ajax({
 
-    })
+          url: "https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=87b934b40ebd6d2f774995a2551013d1",
+          type: "GET",
+          dataType: "json",
+
+
+
+
+
+
+
+
+
+        }).then(
+
+           (data) =>{
+             console.log(data);
+
+            },
+            (error)=>{
+              console.log("bad request");
+            });
+
+
+
+
+      });
+});
+
+
+
+
+
+
+
+
+
+// unction(data){
+//   const brewery_data = " ";
+//   $.each(data, function(key, value){
+//       brewery_data += "<tr>";
+//       brewery_data += "<td>" +value.name+ "</td>";
+//       brewery_data += "<td>" +value.description+ "</td>";
+//       brewery_data += "<td>" +value.established+ "</td>";
+//       brewery_data += "<td>" +value.img+ "</td>";
+//       brewery_data += "<td>" +value.website+ "</td>";
+//       brewery_data += "<td>" +value.region+ "</td>";
+//       brewery_data += "</tr>"
+//   });
+//   $("#brewery_table").append(brewery_data);
+// }
