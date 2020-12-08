414672c3d87aa14344bb90527bd9c8e6079f379f
Andrett Duvenhage <andrettiduv@gmail.com>
2020-12-08 13:32:36 -0800

    code done

--- what-to-watch/app.js
+++ what-to-watch/app.js
@@ -1,33 +1,56 @@
 $(() => {
   // event handler
    $("form").on("submit",(event)=>{
-     //event.preventDefault();
+     event.preventDefault();
 
      const userInput = $("#userInput").val(); // .val grab of input
 
-     $.ajax({ //object is the url
-       url: `https://www.omdbapi.com/?apikey=53aa2cd6&t=${userInput}`,
+  $.ajax({ //object is the url
+    url: `https://www.omdbapi.com/?apikey=53aa2cd6&t=${userInput}`,
      }).then(   //a method that takes two arguments/ promisce
        (data) => {  // var() with 2 arguments
           // handler for success
-         console.log(data);
-         let title = data.Title;
-         $("#title").html(title);
-           //console.log(title);// rmbr API use.Capital letter after data .html(data.Title)    open in moudal
-         $("#year").html(data.Year)//.appendTo($("#modal-textbox"));
-         $("#rated").html(data.Rated);
-         $("#genre").html(data.Genre);
+
+
+//movie & tv data to html
+// ===============================
+        let title = data.Title;
+          $("#title").html(title);
+           //API use.Capital letter after data
+        let director = data.Director;
+           $("#director").html(director);
+
+        let actors = data.Actors;
+           $("#actors").html(actors)
+
+        let genre = data.Genre;
+           $("#genre").html(genre);
+
+        let year = data.Year;
+           $("#year").html(year);
+
+        let rated = data.Rated
+           $("#rated").html(rated);
+
+        let type = data.Type
+           $("#type").html(type);
+
+        let runtime = data.Runtime
+           $("#runtime").html(runtime);
+            console.log(data);
        },
        (error) => {
           // handler for errors
           console.log(error);
-          //console.log(`${error.statusText.ToUpperCase()} :bad request, check url`);
+
 
   });
 
  });
-///modal////
 
+
+///modal popup
+//================
 //Grabbing Elements
 const $openBtn = $("#openModal");
 const $modal = $("#modal");
@@ -40,10 +63,8 @@ const openModal = () => {
 const closeModal = () => {
  $modal.css("display", "none");
 }
-
 //Event Listeners
 $openBtn.on("click", openModal);
-
 $closeBtn.on('click', closeModal);
 
 // time modal open auto
